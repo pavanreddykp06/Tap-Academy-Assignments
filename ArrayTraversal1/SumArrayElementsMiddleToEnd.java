98374f1ef1369e888474b9c9ba2540fb7eefef4d
Pavan Reddy <pavanreddykp8999@gmail.com>
2026-03-11 22:40:57 +0530

    Updated ArrayTraversal1

---
+++ ArrayTraversal1/SumArrayElementsMiddleToEnd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumArrayElementsMiddleToEnd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+       calculateSum(arr);
+        
+    }
+
+    public static void calculateSum(int[] arr) {
+        int sum = 0;
+        for(int i=arr.length/2; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
